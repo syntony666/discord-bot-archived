04710c65d02e7db78b4d80ddddf1b34c1ea50181
柏鴻 呂 <syntony666@outlook.com>
2020-06-18 17:59:21 +0800

    connect to db

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-var fs = require('fs')
+const MongoDB = require('mongodb').MongoClient;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -23,18 +23,22 @@ bot.on('guildMemberAdd', function (member) {
     userID = member.id; 
     console.dir("User ID: "+userID); 
     var serverID = member.guild_id;
-    var welcome = JSON.parse(fs.readFileSync('./response.json','utf8'))["welcome"]
-    for (let index = 0; index < welcome.length; index++) {
-        if(serverID == welcome[index]["server"]){
-            eval('var mes='+welcome[index]["message"]);
-            bot.sendMessage({
-                to: welcome[index]["channel"], 
-                message: mes
-            }); 
-        }
-    }
+    MongoDB.connect(auth.uri,function(err,db){
+        if(err) throw err;
+        db.collection('welcome').find({server : serverID}).toArray(function(err,items){
+            if(err) throw err;
+            if(items.length!=0){
+                eval('var mes='+items[0]["message"]);
+                bot.sendMessage({
+                    to: items[0]["channel"], 
+                    message: mes
+                });
+            }});
+        db.close();
+    });
 });
 bot.on("message", function (user, userID, channelID, message, evt) {
+MongoDB.connect(auth.uri,function(err,db){if(err) throw err;
     var serverID = bot.channels[channelID].guild_id;
     console.log('<' + channelID +'> ' + user + '(' + userID + '): ' + message)
     if (message.substring(0, 1) == '>') {
@@ -48,15 +52,23 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                 });
                 break;
             case 'list':
-                var keyword = JSON.parse(fs.readFileSync('./response.json','utf8'))["keyword"]
-                for (let index = 0; index < keyword.length; index++) {
-                    if(serverID == keyword[index]["server"]){
-                        bot.sendMessage({
-                            to: channelID,
-                            message: '> ' + keyword[index]["receive"] +'\n'+ keyword[index]["send"]
+                if(err) throw err;
+                db.collection('keywords').find({server : serverID}).toArray(function(err,items){if(err) throw err;
+                    if(items.length!=0){
+                        items.forEach(keywords => {
+                            bot.sendMessage({
+                                to: channelID, 
+                                message: '> ' + keywords["receive"] +'\n'+ keywords["send"]
+                            }); 
                         });
                     }
-                }
+                    else
+                    bot.sendMessage({
+                        to: channelID, 
+                        message: '目前沒記任何東西 >_<'
+                    }); 
+
+                });
                 break;
             case 'draw':            // >draw max min
                 if(args[1]!=undefined){
@@ -74,35 +86,19 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                 }
                 break;
             case 'teach':
-                if(args[1]!=undefined){
-                    fs.readFile('./response.json', 'utf8', 
-                        function readFileCallback(err, data){
-                            if (err){
-                                console.log(err);
-                            } 
-                            else {
-                                obj = JSON.parse(data);
-                                for (let index = 0; index < keyword.length; index++) {
-                                    if(args[1] == obj["keyword"][index]["receive"])
-                                        delete obj["keyword"][index]
-                                }
-                                mes = ''
-                                for(let i=2;i < args.length; i++){
-                                    if(i!=2)
-                                        mes+=' ';
-                                    mes+=args[i];
-                                }
-                                obj["keyword"].push({server: serverID, user: userID, receive: args[1], send: mes}); //add some data
-                                obj["keyword"] = obj["keyword"].filter(function(x) { return x !== null }); 
-                                json = JSON.stringify(obj, null, '\t');
-                                fs.writeFile('./response.json', json, 'utf8',function(err){}); 
-                                bot.sendMessage({
-                                    to: channelID,
-                                    message: '<@'+userID+'> 教我聽到人家說 '+ args[1]+' 要回答 '+ mes
-                                });
-                            }
-                        }
-                    );
+                if(args[1]!=undefined){ 
+                    db.collection('keywords').remove({server: serverID, receive: args[1]})
+                    let mes =''
+                    for(let i=2;i < args.length; i++){
+                        if(i!=2)
+                            mes+=' ';
+                        mes+=args[i];
+                    }
+                    db.collection('keywords').insert({server: serverID, user: userID, receive: args[1], send: mes})
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '<@'+userID+'> 教我聽到人家說 '+ args[1]+' 要回答 '+ mes
+                    });
                 }
                 else{
                     bot.sendMessage({
@@ -113,27 +109,11 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                 break;
             case 'delete':
                 if(args[1]!=undefined){
-                    fs.readFile('./response.json', 'utf8', 
-                        function readFileCallback(err, data){
-                            if (err){
-                                console.log(err);
-                            } 
-                            else {
-                                obj = JSON.parse(data);
-                                for (let index = 0; index < keyword.length; index++) {
-                                    if(args[1] == obj["keyword"][index]["receive"])
-                                        delete obj["keyword"][index]
-                                }
-                                obj["keyword"] = obj["keyword"].filter(function(x) { return x !== null }); 
-                                json = JSON.stringify(obj, null, '\t');
-                                fs.writeFile('./response.json', json, 'utf8',function(err){}); 
-                                bot.sendMessage({
-                                    to: channelID,
-                                    message: '當你說'+ args[1]+'的時候 我不會理你'
-                                });
-                            }
-                        }
-                    );
+                        db.collection('keywords').remove({server: serverID, receive: args[1]})
+                        bot.sendMessage({
+                            to: channelID,
+                            message: '當你說'+ args[1]+'的時候 我不會理你'
+                        });
                 }
                 else{
                     bot.sendMessage({
@@ -143,15 +123,14 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                 }
         }
     }
-    var keyword = JSON.parse(fs.readFileSync('./response.json','utf8'))["keyword"]
     if(userID !== bot.id)
-        for (let index = 0; index < keyword.length; index++) {
-            if(message == keyword[index]["receive"] && serverID == keyword[index]["server"]){
+        db.collection('keywords').find({server: serverID, receive: message}).toArray(function(err,items){if(err) throw err;
+            if(items.length!=0)
                 bot.sendMessage({
                     to: channelID,
-                    message: keyword[index]["send"]
+                    message: items[0]["send"]
                 });
-                break;
-            }
-        }
+        });  
+db.close();
+});
 });
--- test.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs')
-var obj = JSON.parse(fs.readFileSync('./response.json','utf8'))
-obj["keyword"].push({"receive":"hohoho","send":"'hahaha'"});
-fs.writeFile('response.json',JSON.stringify(obj))
-console.log(obj)
