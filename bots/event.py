534b3f9b1fe2f2c49b5125eab985992592e2b358
呂柏鴻 <tony@Tony-MacBook-Pro.local>
2020-06-29 21:47:54 +0800

    console print

--- bots/event.py
+++ bots/event.py
@@ -17,7 +17,7 @@ class Event(Extension):
     @commands.Cog.listener()
     async def on_message(self, message):
         print(f'%c${message.author}({message.guild}, #{message.channel}): ')
-        print(msg.content)
+        print(message.content)
         print(f'Message from {message.author}: {message.content}')
         if message.author == self.bot.user:
             return
