0e4d147340339253020acf1a304ad13b09431809
呂柏鴻 <tony@Tony-MacBook-Pro.local>
2020-07-09 22:38:59 +0800

    add self

--- core/extension.py
+++ core/extension.py
@@ -5,20 +5,20 @@ class Extension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@bot.command()
-@commands.is_owner()
-async def load(ctx, extension):
-    bot.load_extension(f'{ext_path}.{name}')
-    await ctx.send(f'loaded {extension} done.')
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        bot.load_extension(f'{ext_path}.{name}')
+        await ctx.send(f'loaded {extension} done.')
 
-@bot.command()
-@commands.is_owner()
-async def unload(ctx, extension):
-    bot.unload_extension(f'{ext_path}.{name}')
-    await ctx.send(f'unloaded {extension} done.')
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx, extension):
+        bot.unload_extension(f'{ext_path}.{name}')
+        await ctx.send(f'unloaded {extension} done.')
 
-@bot.command()
-@commands.is_owner()
-async def reload(ctx, extension):
-    bot.reload_extension(f'{ext_path}.{name}')
-    await ctx.send(f'reloaded {extension} done.')
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, extension):
+        bot.reload_extension(f'{ext_path}.{name}')
+        await ctx.send(f'reloaded {extension} done.')
