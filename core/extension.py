172af1cc218727e462e53a3389390a41a3cc7772
呂柏鴻 <tony@Tony-MacBook-Pro.local>
2020-07-09 22:44:38 +0800

    new load.py

--- core/extension.py
+++ core/extension.py
@@ -4,21 +4,3 @@ from discord.ext import commands
 class Extension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    @commands.is_owner()
-    async def load(self, ctx, extension):
-        bot.load_extension(f'{ext_path}.{name}')
-        await ctx.send(f'loaded {extension} done.')
-
-    @commands.command()
-    @commands.is_owner()
-    async def unload(self, ctx, extension):
-        bot.unload_extension(f'{ext_path}.{name}')
-        await ctx.send(f'unloaded {extension} done.')
-
-    @commands.command()
-    @commands.is_owner()
-    async def reload(self, ctx, extension):
-        bot.reload_extension(f'{ext_path}.{name}')
-        await ctx.send(f'reloaded {extension} done.')
