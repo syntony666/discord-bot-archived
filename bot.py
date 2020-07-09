b4ab3b477cd9469270cc3508ae93e5d6b2318cae
呂柏鴻 <tony@Tony-MacBook-Pro.local>
2020-07-09 22:37:12 +0800

    file clean

--- bot.py
+++ bot.py
@@ -1,39 +1,18 @@
 import discord
 from discord.ext import commands
-from pymongo import MongoClient
 from core.bot_db import MongoDB as mongo
 import os
 
 # from exceptions import Exceptions
 
-ext_path = 'bots'
+ext_path = 'ext'
 db = mongo()
-# auth = db['auth'].find_one()
 bot = commands.Bot(command_prefix=db.auth('prefix'))
 
-@bot.command()
-@commands.is_owner()
-async def load(ctx, extension):
-    bot.load_extension(f'{ext_path}.{name}')
-    await ctx.send(f'loaded {extension} done.')
-
-@bot.command()
-@commands.is_owner()
-async def unload(ctx, extension):
-    bot.unload_extension(f'{ext_path}.{name}')
-    await ctx.send(f'unloaded {extension} done.')
-
-@bot.command()
-@commands.is_owner()
-async def reload(ctx, extension):
-    bot.reload_extension(f'{ext_path}.{name}')
-    await ctx.send(f'reloaded {extension} done.')
-
-for file in os.listdir('./'+ext_path):
+for file in os.listdir('./'+ ext_path):
     if file.endswith(".py"):
         name = file[:-3]
         bot.load_extension(f'{ext_path}.{name}')
 
 if __name__ == "__main__":
-    print(db.auth('token'))
     bot.run(db.auth('token'))
--- ext/command.py
+++ ext/command.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from pymongo import MongoClient
-from extension import Extension
+from core.extension import Extension
 
 db = MongoClient('mongodb://syntony666:tony738294@ds027519.mlab.com:27519/heroku_vfz6lbdq?retryWrites=false').heroku_vfz6lbdq
 keywords = db['keywords']
