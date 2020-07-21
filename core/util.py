7a38c3e674a3558d34d7ebe6a13250138f90bfea
Tony <syntony666@outlook.com>
2020-07-21 11:15:51 +0800

    delete senstive info

--- core/util.py
+++ core/util.py
@@ -2,8 +2,10 @@ from pymongo import MongoClient
 import os
 
 def getDatabase():
+    # get variable from heroku var
     username = os.environ.get('username')
     password = os.environ.get('password')
     dbname = os.environ.get('dbName')
+    # access data from MongoDB
     db = MongoClient(f'mongodb+srv://{username}:{password}@discordbotdb-iopzr.gcp.mongodb.net/{dbname}?retryWrites=true&w=majority')
     return db[str(dbname)]
