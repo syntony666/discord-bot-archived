002aceec1b09f6a2d7b2a243fb1bdc6ec5a4a677
Tony <syntony666@outlook.com>
2020-07-21 11:02:32 +0800

    try push

--- test.py
+++ test.py
@@ -6,12 +6,15 @@ import os
 
 db = getDatabase()
 
-collection = db['welcome']
+collection = db['keywords']
 
 print('connected')
 
 
-post = {    "server": "438997365194489856",
-    "channel": "676414129145249792",
-    "message": "歡迎加入，請問是玩什麼遊戲呢?"}
+post = {"server": "438997365194489856",
+    "user": "328928072239939584",
+    "receive": "樂高",
+    "send": "好樂高！ 樂高的奧妙之處，是可以藏於眾多玩具之中，隨手可得，還可以組裝它掩藏殺機。 就算被警察抓也告不了你，真不愧為七種刑具之首！"}
 collection.insert_one(post)
+
+print('post success')
