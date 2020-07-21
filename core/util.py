090210a56bc2947aa34ee11dd2246527988dcab3
Tony <syntony666@outlook.com>
2020-07-21 13:23:45 +0800

    test

--- core/util.py
+++ core/util.py
@@ -6,6 +6,7 @@ def getDatabase():
     username = str(os.environ.get('username'))
     password = str(os.environ.get('password'))
     dbname = str(os.environ.get('dbName'))
+    print(os.environ.get('username'))
     # access data from MongoDB
     db = MongoClient(f'mongodb+srv://{username}:{password}@discordbotdb-iopzr.gcp.mongodb.net/{dbname}?retryWrites=true&w=majority')
     return db[dbname]
