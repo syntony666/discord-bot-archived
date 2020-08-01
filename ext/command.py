838be7e45c095b5e788688bfaaaa803ac1f62051
Tony <syntony666@outlook.com>
2020-08-02 00:26:17 +0800

    fix bug

--- ext/command.py
+++ ext/command.py
@@ -58,7 +58,7 @@ class Command(Extension):
         found = self.db['welcome'].find_one({'server' : server})
         await ctx.channel.purge(limit = 1)
         if found is not None:
-            self.db['welcome'].find_one_and_update({'server' : server, 'message': welmes})
+            self.db['welcome'].find_one_and_update({'server' : server},{'$set':{'message': welmes}})
             await ctx.send(f'{ctx.message.guild} 的歡迎訊息為 {welmes}')
             return
         await ctx.send(f'未設定歡迎訊息')
