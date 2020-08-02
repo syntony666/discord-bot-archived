c4b3133e23af60526b08e3e5154ddbfbd7a8e255
Tony <syntony666@outlook.com>
2020-08-02 21:37:06 +0800

    set os enviro

--- core/util.py
+++ core/util.py
@@ -3,9 +3,9 @@ import os
 
 def getDatabase():
     # get variable from heroku var
-    username = str(os.environ.get('username'))
-    password = str(os.environ.get('password'))
-    dbname = str(os.environ.get('dbName'))
+    username = str(os.environ.get('MONGODB_USERNAME'))
+    password = str(os.environ.get('MONGODB_PASSWORD'))
+    dbname = 'bot-db'
     # access data from MongoDB
     db = MongoClient(f'mongodb+srv://{username}:{password}@discordbotdb-iopzr.gcp.mongodb.net/{dbname}?retryWrites=true&w=majority')
     return db[dbname]
