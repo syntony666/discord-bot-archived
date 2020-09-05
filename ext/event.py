a2e0366655ad368665b829821aebd7794c88a57e
Tony <syntony666@outlook.com>
2020-09-05 17:32:20 +0800

    fix bug in member join and remove

--- ext/event.py
+++ ext/event.py
@@ -23,16 +23,16 @@ class Event(Extension):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         welcome = self.db['config'].find_one({'server': member.guild.id})
-        if welcome["leave"]["channel"] != 0:
-            await self.bot.get_channel(welcome["leave"]["channel"])\
-                .send(f'{member} {welcome["leave"]["message"]}')
+        if welcome["welcome"]["channel"] != 0:
+            await self.bot.get_channel(welcome["welcome"]["channel"])\
+                .send(f'{member.mention} {welcome["welcome"]["message"]}')
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         welcome = self.db['config'].find_one({'server': member.guild.id})
-        if welcome["welcome"]["channel"] != 0:
-            await self.bot.get_channel(welcome["welcome"]["channel"])\
-                .send(f'{member.mention} {welcome["welcome"]["message"]}')
+        if welcome["leave"]["channel"] != 0:
+            await self.bot.get_channel(welcome["leave"]["channel"])\
+                .send(f'{member} {welcome["leave"]["message"]}')
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -41,7 +41,7 @@ class Event(Extension):
             'welcome': {'channel': 0, 'message': ''},
             'leave': {'channel': 0, 'message': ''}
         }
-        self.db['config'].insert(configData)
+        self.db['config'].insert_one(configData)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
