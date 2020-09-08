d6f75ff3cf32ff5921e6014f5be76f9904a0e197
Tony <syntony666@outlook.com>
2020-09-09 01:09:37 +0800

    add some message when add reminder

--- ext/command.py
+++ ext/command.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timezone, timedelta
-
 import discord
 from discord.ext import commands
 
--- ext/reminder.py
+++ ext/reminder.py
@@ -1,7 +1,7 @@
 import asyncio
 import datetime
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from discord.ext import commands
 
@@ -62,41 +62,80 @@ class Reminder(Extension):
         self.invokedNoSubcommand(ctx)
 
     @reminder.command(aliases=['rd', 'day'])
-    async def repeat_by_day(self, ctx, hour: int, minute: int, title, *, msg):
+    async def repeat_by_day(self, ctx: commands.Context, hour: int, minute: int, title, *, msg):
+        if self.db['reminder'].find_one({"server": ctx.guild.id, "title": title}) is not None:
+            await ctx.send(f'標題名稱相同，無法建立提醒')
+            return
+        setTime = datetime(2020, 1, 1, hour=hour, minute=minute)
         self.db['reminder'].insert_one({
             "server": ctx.guild.id,
             "channel": ctx.channel.id,
             "repeat": "day",
-            "time": datetime(2020, 1, 1, hour=hour, minute=minute).strftime('%d%H%M'),
+            "time": setTime.strftime('%H%M'),
             "title": title,
             "message": msg
         })
+        embed = {
+            "title": title,
+            "description": '',
+            "context": {
+                "時間": setTime.strftime("每天 %H:%M"),
+                "訊息": msg
+            }
+        }
+        await ctx.send(f'{ctx.author.mention} 建立了一個提醒',
+                       embed=self.setEmbedList(embed["title"], embed["description"], embed["context"]))
 
     @reminder.command(aliases=['rm', 'month'])
     async def repeat_by_month(self, ctx, day: int, hour: int, minute: int, title, *, msg):
+        if self.db['reminder'].find_one({"server": ctx.guild.id, "title": title}) is not None:
+            await ctx.send(f'標題名稱相同，無法建立提醒')
+            return
+        setTime = datetime(2020, 1, day, hour=hour, minute=minute)
         self.db['reminder'].insert_one({
             "server": ctx.guild.id,
             "channel": ctx.channel.id,
             "repeat": "month",
-            "time": datetime(2020, 1, day, hour=hour, minute=minute).strftime('%H%M'),
+            "time": setTime.strftime('%d%H%M'),
             "title": title,
             "message": msg
         })
+        embed = {
+            "title": title,
+            "description": '',
+            "context": {
+                "時間": setTime.strftime("每個月%d號 %H:%M"),
+                "訊息": msg
+            }
+        }
+        await ctx.send(f'{ctx.author.mention} 建立了一個提醒',
+                       embed=self.setEmbedList(embed["title"], embed["description"], embed["context"]))
 
     @reminder.command(aliases=['o', 'once'])
-    async def just_remind_once(self, ctx, year: int, month: int, day: int, hour: int, minute: int, title, *, msg):
+    async def just_remind_once(self, ctx, year: int, month: int, day: int, hour: int, minute: int, *, msg):
         setTime = datetime(year, month, day, hour=hour, minute=minute, second=0)
         if datetime.now() > setTime:
             await ctx.send('時間不會回溯')
             return
+        if datetime.now() + timedelta(days=30) < setTime:
+            await ctx.send('不接受30天後的提醒')
         self.db['reminder'].insert_one({
             "server": ctx.guild.id,
             "channel": ctx.channel.id,
             "repeat": "none",
             "time": setTime.strftime('%Y%m%d%H%M'),
-            "title": title,
             "message": msg
         })
+        embed = {
+            "title": '提醒',
+            "description": '',
+            "context": {
+                "時間": setTime.strftime("%Y/%m/%d %H:%M"),
+                "訊息": msg
+            }
+        }
+        await ctx.send(f'{ctx.author.mention} 建立了一個提醒',
+                       embed=self.setEmbedList(embed["title"], embed["description"], embed["context"]))
 
     @reminder.command(alias=['d', 'delete'])
     async def delete_reminders(self, ctx, title):
