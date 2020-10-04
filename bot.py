ac669710874a09d7d6ed6358991f09d2e733cd80
Tony <syntony666@outlook.com>
2020-10-04 20:37:11 +0800

    update to 1.5

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 
+import discord
 from discord.ext import commands
 
 from core.util import getDatabase
@@ -7,7 +8,8 @@ from core.util import getDatabase
 ext_path = 'ext'
 db = getDatabase()
 auth = db['auth'].find_one()
-bot = commands.Bot(command_prefix=auth['prefix'], help_command=None)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=auth['prefix'], help_command=None, intents=intents)
 
 for file in os.listdir('./' + ext_path):
     if file.endswith(".py"):
