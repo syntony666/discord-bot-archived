713c0d7a354b2fcc8a54f9b2bd7af38f5b6e81b9
Tony <syntony666@outlook.com>
2020-12-21 15:35:11 +0800

    temporarily close gamble

--- ext/money.py
+++ ext/money.py
@@ -15,50 +15,52 @@ class Money(Extension):
 
     @commands.command()
     async def dice(self, ctx, num: int):
-        member = Member(ctx.author.id, ctx.guild.id)
-        cost, reward = 50, 300
-        if num < 1 or num > 6:
-            await ctx.send(f'不要亂玩啦!! ><')
-            return
-        elif member.get_cash() >= cost:
-            dice = random.randint(1, 6)
-            cash = member.get_cash()
-            send_str = f'你骰到 {dice} '
-            if dice == num:
-                cash += reward
-                send_str += f'獲得現金 {reward}'
-            else:
-                cash -= cost
-                send_str += f'損失現金 {cost}'
-            member.set_cash(cash)
-            member.update()
-            await ctx.send(f'{send_str}, 你還有現金 {member.get_cash()}')
-        else:
-            await ctx.send(f'沒錢了 還想賭阿')
+        print('此功能目前維護中')
+        # member = Member(ctx.author.id, ctx.guild.id)
+        # cost, reward = 50, 300
+        # if num < 1 or num > 6:
+        #     await ctx.send(f'不要亂玩啦!! ><')
+        #     return
+        # elif member.get_cash() >= cost:
+        #     dice = random.randint(1, 6)
+        #     cash = member.get_cash()
+        #     send_str = f'你骰到 {dice} '
+        #     if dice == num:
+        #         cash += reward
+        #         send_str += f'獲得現金 {reward}'
+        #     else:
+        #         cash -= cost
+        #         send_str += f'損失現金 {cost}'
+        #     member.set_cash(cash)
+        #     member.update()
+        #     await ctx.send(f'{send_str}, 你還有現金 {member.get_cash()}')
+        # else:
+        #     await ctx.send(f'沒錢了 還想賭阿')
 
     @commands.command()
     async def guess(self, ctx, guess):
-        member = Member(ctx.author.id, ctx.guild.id)
-        random.seed(time.process_time())
-        cost, reward = 20, 50
-        if guess != 'b' and guess != 's':
-            await ctx.send(f'猜大(1~3)輸入"b", 猜小(4~6)輸入"s", 輸入其他東西沒用')
-            return
-        elif member.get_cash() >= cost:
-            dice = random.randint(1, 6)
-            send_str = f'你骰到 {dice} 是大' if dice > 3 else f'你骰到 {dice} 是小 '
-            cash = member.get_cash()
-            if dice > 3 and guess == 'b' or dice < 4 and guess == 's':
-                cash += reward
-                send_str += f'獲得現金 {reward}'
-            else:
-                cash -= cost
-                send_str += f'損失現金 {cost}'
-            member.set_cash(cash)
-            member.update()
-            await ctx.send(f'{send_str}, 你還有現金 {member.get_cash()}')
-        else:
-            await ctx.send(f'沒錢了 還想賭阿')
+        print('此功能目前維護中')
+        # member = Member(ctx.author.id, ctx.guild.id)
+        # random.seed(time.process_time())
+        # cost, reward = 20, 50
+        # if guess != 'b' and guess != 's':
+        #     await ctx.send(f'猜大(1~3)輸入"b", 猜小(4~6)輸入"s", 輸入其他東西沒用')
+        #     return
+        # elif member.get_cash() >= cost:
+        #     dice = random.randint(1, 6)
+        #     send_str = f'你骰到 {dice} 是大' if dice > 3 else f'你骰到 {dice} 是小 '
+        #     cash = member.get_cash()
+        #     if dice > 3 and guess == 'b' or dice < 4 and guess == 's':
+        #         cash += reward
+        #         send_str += f'獲得現金 {reward}'
+        #     else:
+        #         cash -= cost
+        #         send_str += f'損失現金 {cost}'
+        #     member.set_cash(cash)
+        #     member.update()
+        #     await ctx.send(f'{send_str}, 你還有現金 {member.get_cash()}')
+        # else:
+        #     await ctx.send(f'沒錢了 還想賭阿')
 
     @commands.command(aliases=['daily'])
     async def daily_cash(self, ctx):
