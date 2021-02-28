e51ae8f7586b35716a6a11a55e266569f432722d
Tony <syntony666@outlook.com>
2021-02-28 22:49:51 +0800

    test reply l

--- cmds/reply.py
+++ cmds/reply.py
@@ -41,6 +41,7 @@ class Reply(Extension):
             return
         embed = discord.Embed(title='__回應列表__', color=0x3ea076)
         reply_list = [{x: y} for x, y in reply_list]
+        print(reply_list)
         embed_page = EmbedPage(embed, reply_list, 10)
         await embed_page.run(self.bot, ctx)
         # reply_list = [reply_list[i:i + 10] for i in range(0, len(reply_list), 10)]
