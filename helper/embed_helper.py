49b13f3bc8f871ca3312663af46bd6a96fe6427b
Tony <syntony666@outlook.com>
2021-02-28 23:24:28 +0800

    test reply l

--- helper/embed_helper.py
+++ helper/embed_helper.py
@@ -6,23 +6,27 @@ import discord
 class EmbedPage:
     def __init__(self, embed: discord.Embed, data: list, page_num: int):
         self.data = [data[i:i + page_num] for i in range(0, len(data), page_num)]
-        self.embed = embed
+        self.embed = list()
+        for n in range(len(self.data)):
+            self.embed += embed
+            for x in self.data[n]:
+                self.embed[n].add_field(name=x['key'], value=x['value'], inline=False)
         self.now_page = 1
-        self.max_page = len(data)
+        self.max_page = len(self.embed)
 
-    def set_page(self, page):
-        self.now_page = page
-        self.embed.clear_fields()
-        for x in self.data[self.now_page - 1]:
-            self.embed.add_field(name=x['key'], value=x['value'], inline=False)
-        self.embed.set_footer(text=f'Page {self.now_page}/{self.max_page}')
+    # def set_page(self, page):
+    #     self.now_page = page
+    #     self.embed.clear_fields()
+    #     for x in self.data[self.now_page - 1]:
+    #         self.embed.add_field(name=x['key'], value=x['value'], inline=False)
+    #     self.embed.set_footer(text=f'Page {self.now_page}/{self.max_page}')
 
     async def run(self, bot: discord.Client, ctx, delay=30):
         def check(reaction, user):
             return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
 
-        self.set_page(self.now_page)
-        message = await ctx.send(embed=self.embed)
+        # self.set_page(self.now_page)
+        message = await ctx.send(embed=self.embed[0])
 
         await message.add_reaction("◀️")
         await message.add_reaction("▶️")
@@ -31,13 +35,15 @@ class EmbedPage:
             while True:
                 reaction, user = await bot.wait_for("reaction_add", timeout=delay, check=check)
                 if str(reaction.emoji) == "▶️" and self.now_page != self.max_page:
-                    self.set_page(self.now_page + 1)
+                    self.now_page += 1
+                    # self.set_page(self.now_page + 1)
                     await message.remove_reaction(reaction, user)
-                    await message.edit(embed=self.embed)
+                    await message.edit(embed=self.embed[self.now_page-1])
                 elif str(reaction.emoji) == "◀️" and self.now_page > 1:
-                    self.set_page(self.now_page - 1)
+                    self.now_page -= 1
+                    # self.set_page(self.now_page - 1)
                     await message.remove_reaction(reaction, user)
-                    await message.edit(embed=self.embed)
+                    await message.edit(embed=self.embed[self.now_page-1])
                 else:
                     await message.remove_reaction(reaction, user)
         except asyncio.TimeoutError:
