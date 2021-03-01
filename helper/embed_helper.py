d6196c52c400a60a70e6f90704fc74011b62bb6b
Tony <syntony666@outlook.com>
2021-03-01 11:48:36 +0800

    test reply l

--- helper/embed_helper.py
+++ helper/embed_helper.py
@@ -6,29 +6,31 @@ import discord
 class EmbedPage:
     def __init__(self, embed: discord.Embed, data: list, page_num: int):
         self.data = [data[i:i + page_num] for i in range(0, len(data), page_num)]
-        self.embed = list()
-        print(len(self.data))
-        for n in range(len(self.data)):
-            self.embed.append(embed)
-            for x in self.data[n]:
-                self.embed[n].add_field(name=x['key'], value=x['value'], inline=False)
-            print(self.embed[n].to_dict())
+        self.embed = embed
+        # self.embed = list()
+        # print(len(self.data))
+        # for n in range(len(self.data)):
+        #     self.embed.append(embed)
+        #     for x in self.data[n]:
+        #         self.embed[n].add_field(name=x['key'], value=x['value'], inline=False)
+        #     print(self.embed[n].to_dict())
         self.now_page = 1
         self.max_page = len(self.embed)
 
-    # def set_page(self, page):
-    #     self.now_page = page
-    #     self.embed.clear_fields()
-    #     for x in self.data[self.now_page - 1]:
-    #         self.embed.add_field(name=x['key'], value=x['value'], inline=False)
-    #     self.embed.set_footer(text=f'Page {self.now_page}/{self.max_page}')
+    def set_page(self, page):
+        self.now_page = page
+        self.embed.clear_fields()
+        for x in self.data[self.now_page - 1]:
+            self.embed.add_field(name=x['key'], value=x['value'], inline=False)
+        self.embed.set_footer(text=f'Page {self.now_page}/{self.max_page}')
 
     async def run(self, bot: discord.Client, ctx, delay=30):
         def check(reaction, user):
             return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
 
-        # self.set_page(self.now_page)
-        message = await ctx.send(embed=self.embed[0])
+        self.set_page(self.now_page)
+        # print(self.embed[0])
+        message = await ctx.send(embed=self.embed)
 
         await message.add_reaction("◀️")
         await message.add_reaction("▶️")
@@ -38,8 +40,8 @@ class EmbedPage:
                 reaction, user = await bot.wait_for("reaction_add", timeout=delay, check=check)
                 if str(reaction.emoji) == "▶️" and self.now_page != self.max_page:
                     self.now_page += 1
-                    # self.set_page(self.now_page + 1)
-                    await message.remove_reaction(reaction, user)
+                    self.set_page(self.now_page + 1)
+                    # await message.remove_reaction(reaction, user)
                     await message.edit(embed=self.embed[self.now_page-1])
                 elif str(reaction.emoji) == "◀️" and self.now_page > 1:
                     self.now_page -= 1
