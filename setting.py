928a172efe8f9565fc3c84357a35b2cfb752a550
Tony <syntony666@outlook.com>
2021-03-02 10:01:36 +0800

    bug solved

--- setting.py
+++ setting.py
@@ -1,7 +1,6 @@
 import os
 
-DATABASE_URL = 'mongodb+srv://admin:M9IHInqE44aRmAXI@discordbotdb.iopzr.mongodb.net/db?retryWrites=true&w=majority'
-# DATABASE_URL = os.environ.get('DB_URL')
+DATABASE_URL = os.environ.get('DB_URL')
 
 STAT_ROLE = {
     'Vtuber': 785901109071839244,
