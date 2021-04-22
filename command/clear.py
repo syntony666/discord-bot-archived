cb539e3f05eeb2b230d797ffb6b16cff7d1b9009
Tony <syntony666@outlook.com>
2021-04-22 23:16:07 +0800

    change prefix and add clear command

---
+++ command/clear.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from discord import Embed, Color
+from discord.ext import commands
+
+from duration_service import DurationService
+from extension import Extension
+
+
+class Clear(Extension):
+    @commands.group()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx):
+        pass
+
+    @clear.command(aliases=['time'])
+    async def clear_by_time(self, ctx, year: int, month: int, day: int, hour: int = 0, minute: int = 0):
+        purge_time = datetime(year, month, day, hour=hour, minute=minute)
+        await ctx.channel.purge(after=purge_time - timedelta(hours=8), limit=10000)
+        await send_embed_msg(ctx, f'刪除了 **{purge_time.strftime("%Y/%m/%d %H:%M")}** 後的訊息')
+
+    @clear.command(aliases=['dur'])
+    async def clear_by_duration(self, ctx, duration):
+        purge_time = datetime.now() - DurationService(duration).get_time()
+        await ctx.channel.purge(after=purge_time - timedelta(hours=8), limit=1000)
+        await send_embed_msg(ctx, f'刪除了 **{purge_time.strftime("%Y/%m/%d %H:%M")}** 後的訊息')
+
+    @clear.command(aliases=['num'])
+    async def clear_by_num(self, ctx, num: int):
+        await ctx.channel.purge(limit=num + 1)
+        await send_embed_msg(ctx, f'刪除了 **{num}** 則訊息')
+
+
+async def send_embed_msg(ctx, description):
+    embed = Embed(title='已刪除訊息', color=Color.gold(), description=description)
+    embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Clear(bot))
---
+++ service/duration_service.py
@@ -0,0 +1,32 @@
+import re
+from datetime import timedelta
+
+from core.exception import FormatError
+
+
+class DurationService:
+    def __init__(self, time):
+        regex = re.compile(r'^((?P<days>\d+?)d)?((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?$')
+        self.time = dict()
+        try:
+            parts = regex.match(time)
+            if parts is None:
+                raise FormatError
+            self.time = {name: int(param) for name, param in parts.groupdict().items() if param}
+        except ValueError:
+            raise FormatError
+
+    def get_time(self):
+        return timedelta(**self.time)
+
+    def get_str(self):
+        time_str = ''
+        if 'days' in self.time.keys():
+            time_str += f' {self.time["days"]}天'
+        if 'hours' in self.time.keys():
+            time_str += f' {self.time["hours"]}小時'
+        if 'minutes' in self.time.keys():
+            time_str += f' {self.time["minutes"]}分'
+        if 'seconds' in self.time.keys():
+            time_str += f' {self.time["seconds"]}秒'
+        return time_str
