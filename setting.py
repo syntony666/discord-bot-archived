44868aa77e3948106ea4d5b485514cd8fd2b1ae7
Tony <syntony666@outlook.com>
2021-05-01 10:15:30 +0800

    debug in validation

--- setting.py
+++ setting.py
@@ -2,11 +2,11 @@
 import os
 
 DATABASE_URL = os.environ.get('DB_URL')
-DATABASE_NAME = 'test'
+DATABASE_NAME = 'dc-bot'
 
 
 # bot arguments
-PREFIX = '>'
+PREFIX = '* '
 TOKEN = os.environ.get('DC_TOKEN')
 
 VERSION = 'ver. 3.0.2 release'
